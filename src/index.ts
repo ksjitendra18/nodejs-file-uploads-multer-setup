f0b172cfc2d6c5b3a992aee11864ef575ee32207
ksjitendra18 <jitendraks1803@gmail.com>
2024-06-21 10:42:37 +0530

    starter-code

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ message: "hello world" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("App running on ", PORT);
+});
