b5b7fad5c1f420892382646d6a6676418e49b408
ksjitendra18 <jitendraks1803@gmail.com>
2024-07-08 16:10:21 +0530

    upload functionality

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,6 @@ app.use(json());
 
 app.post("/upload", multerMiddleware.single("file"), async (req, res) => {
   try {
-    cloudinary.config({
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
-    });
     const file = req.file;
 
     if (!file) {
